34e53c9ef87c6dc8d4c76fdbdc5ed43298ca6b20
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2019-02-27 17:38:39 +0100

    Add project files.

---
+++ BooksApp/Models/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore; //!!!
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApp.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options) //konstruktor
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; } //przekazujemy model do contextu dla db
+    }
+}
